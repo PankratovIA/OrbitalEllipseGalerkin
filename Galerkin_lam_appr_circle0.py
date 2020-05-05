7b975c73907e29f20c59c1e3bd5f8772b0f72efc
PankratovIA <PankratovIA.mechanic@gmail.com>
2020-05-05 23:05:28 +0400

    +graph for error

--- Galerkin_lam_appr_circle0.py
+++ Galerkin_lam_appr_circle0.py
@@ -1,11 +1,11 @@
 from math import sin, cos, pi
-
 import numpy as np
 from MathematicModels import Quaternion
-from scipy.integrate import quad
+from scipy.integrate import quad, odeint, ode
+import matplotlib.pyplot as plt
 
-ez = 0.
-T = 0.1
+ez = 0.01
+T = 4*pi
 N = 1
 # lam0 = Quaternion([1, 0, 0, 0])
 # lam0 = Quaternion([0.8, 0, 0.6, 0])
@@ -79,6 +79,17 @@ def Kvect(phi, s, k, idx):
     # print(ans[idx])
     return ans[idx]
     
+# Cauchy
+def eqEllipse(lam, phi):
+    #print("lam =", lam)
+    lam = Quaternion(lam)
+    r3 = r(phi) ** 3.0
+    omega = Quaternion([0, Nb * r3, 0, 1.0])
+    ans = lam * omega
+    ans = ans * Quaternion([0.5, 0, 0, 0])
+    return [ans[idx] for idx in range(4)]
+        
+    
 if __name__ == "__main__":
     print("Galerkin >>>")    
     print(r(3.14))
@@ -113,11 +124,43 @@ if __name__ == "__main__":
     print("Inv check (1, 0, 0, 0) =", a * K[0][0])
     
     a = f[0] * a 
+    # a = Quaternion([0, 0, 0, 0])
     print("a =", a)
     
     print("lam0 = ", lam0, lam0.getNorm())
     print("lam(0) = ", lam([a], 0))
     print("lam(T) = ", lam([a], T), lam([a], T).getNorm())
+    
+    #Cauchy
+    # circle [0.031046560091828313, 0.031103992683832533, 0.031161212781716421, 0.031218202529294029, 0.031274944260239294]
+    # poly [0.029317544396431784, 0.029377607905505098, 0.029437517515314045, 0.029497254701257254, 0.029556801152425299]
+    # sin [0.029878477588461995, 0.02994193916572772, 0.030005212904268913, 0.030068280403747726, 0.030131123479297209]
+    print("Cauchy >>>")
+    phi = np.linspace(0, T, 1001)
+    sol = odeint(eqEllipse, [lam0[idx] for idx in range(4)], phi, rtol=1e-15)
+    print(sol[:5], "\n\n", sol[-5:])
+    
+    lastQ = Quaternion(sol[-1])
+    print(lastQ, lastQ.getNorm())
+    
+    print(lam([a], T), lam([a], T).getNorm())
+    
+    err = []
+    for cur in zip(phi, sol):
+        l = lam([a], cur[0])
+        diff = Quaternion(cur[1]) - l
+        err.append(diff.getNorm() ** .5)
+        
+    print(err[:5])
+    print(err[-5:])
+    
+    plt.plot(phi, err, label ='err')
+    plt.legend(loc='best')
+    plt.xlabel('phi')
+    plt.grid()
+    plt.show()
+    
+    print("Cauchy <<<")
     
     print("Galerkin <<<")    
 
