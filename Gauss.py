52ede01734964db1715d8c1a2998ea5068b44b84
PankratovIA <PankratovIA.mechanic@gmail.com>
2020-05-06 23:02:44 +0400

    commented lines were removed

--- Gauss.py
+++ Gauss.py
@@ -6,33 +6,16 @@ def makeTriangle(a):
     assert(all([len(row) == N+1 for row in a]))
     for r in range(N-1):
         tmp = a[r][r].getInv()
-        #print("a[{0}] =".format(row))
-        #printQuatMatr(a[row])
         a[r] = [x * tmp for x in a[r]]
-        #print("a[{0}] =".format(row))
-        #printQuatMatr(a[row])
         for j in range(r+1, N):
             tmp = a[j][r]
-#             print("tmp =", str(tmp))
             for col in range(r, N+1):
-#                 print("a[j][col] a[r][col] tmp", str(a[j][col]), str(a[r][col]), str(tmp))
-#                 print("a[r][col] * tmp", str(a[r][col] * tmp))
-                z = a[j][col] - (a[r][col] * tmp)
-#                 print("z", str(z))
-#                 print(" a[j][col] >>>", str(a[j][col]))
-                a[j][col] = z#Quaternion([z[0], z[1], z[2], z[3]])
-#                 print(" a[j][col] <<<", str(a[j][col]))
-                
-#     for i in range(N):
-#         for j in range(N+1):
-#             print("i = {0}, j = {1} {2}".format(i, j, str(a[i][j])))
-#     printQuatMatr(a)
-                
-    return list(a)
+                a[j][col] = a[j][col] - (a[r][col] * tmp)#Quaternion([z[0], z[1], z[2], z[3]])
+
+    return a
 
 def printQuatMatr(a):
     for row in a:
-        #print("row =", row)
         print("  ".join(map(str,row)))
 
 if __name__ == "__main__":
@@ -46,14 +29,9 @@ if __name__ == "__main__":
     x = np.dot(np.linalg.inv(a), b)
     print("x =", x)
     
-    aQ = [list(Quaternion([i+j+1, 0, 0, 0]) for j in range(2)) for i in range(2)]
+    aQ = [list(Quaternion([i+j+2, 0, 0, 0]) for j in range(2)) for i in range(2)]
     aQ[0].append(Quaternion([1, 0, 0, 0]))
     aQ[1].append(Quaternion([1, 0, 0, 0]))
-#     print("aQ =", aQ)
-#     for i in range(2):
-#         print("---")
-#         for j in range(3):
-#             print("i = {0}, j = {1}, {2}".format(i, j, aQ[i][j]))
     
     print("aQ =")
     printQuatMatr(aQ)
